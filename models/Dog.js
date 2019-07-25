ac5080d5511fb85830b9fb09a67a4c97d0e66c76
vairojarela <vairojarela@gmail.com>
2019-07-25 15:49:03 +0200

    Add Shelter and Dog Models

---
+++ models/Dog.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const dogSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  breed: {
+    type: String,
+    default: 'No Breed'
+  },
+  breed: {
+    type: String,
+    default: 'No Breed'
+  },
+  breed: {
+    type: String,
+    default: 'No Breed'
+  },
+  size: [{
+    type: String,
+    enum: ['Small','Medium','Large','Extra Large']
+  }],
+  age: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  status: {
+    type: Array,
+    enum: ['Liked', 'Matched', 'Accepted', 'Adopted']
+  }
+}, {
+  timestamps: true
+})
+
+const Dog = mongoose.model('Dog', dogSchema)
+
+module.exports = Dog
---
+++ models/Shelter.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const shelterSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: Number
+  },
+  email: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  adress: {
+    type: String
+  },
+  dogs: [{
+    type: ObjectId,
+    ref: 'Dog'
+  }],
+  notes: [{
+    type: ObjectId,
+    ref: 'Notes'
+  }]
+}, {
+  timestamps: true
+})
+
+const Shelter = mongoose.model('Shelter', shelterSchema)
+
+module.exports = Shelter
