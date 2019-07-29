1ba207c6b6fdd0517b3d3dfd19970559dcc32917
Pol Ribas <polribasrovira@gmail.com>
2019-07-29 13:20:14 +0200

    axos done

--- routes/api.js
+++ routes/api.js
@@ -6,44 +6,28 @@ const Shelter = require('../models/Shelter.js')
 const Dogs = require('../models/Dog.js')
 const Notes = require('../models/Notes.js')
 
-//algunas rutas json para poder filtrar los perros
-
-
-/* GET home page. */
-// router.post('/recipes', async (req, res, next) => {
-//   const { title, level, cuisine, duration } = req.body
-//   try {
-//     const recipe = await Recipe.create({ title, level, cuisine, duration })
-//     const recipeID = recipe._id
-//     const userID = req.session.currentUser._id
-//     await User.findByIdAndUpdate(userID, { $push: { recipes: recipeID } })
-//     res.json(recipe)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
-// router.post('/recipes/:id/delate', async (req, res, next) => {
-//   const { id } = req.params
-//   await Recipe.findByIdAndDelete(id)
-//   res.json({ message: 'Recipe deleted' })
-// })
-
 router.post('/likeDog', async (req, res, next) => {
     try{
     const id = req.session.currentUser._id
     const username = req.session.currentUser.username
     const shelterId = req.body.shelter
     const dogId = req.body.dog;
+    // if user tiene al perro en like
     const note = await Notes.create({idDog: req.body.dog, idUser: id},{ new: true})
     await Notes.findByIdAndUpdate(note[0]._id,{$push: {message: `${username}: I want to be adopted`}})
     await User.findByIdAndUpdate(id,{$push: {notes: note[0]._id}})
     await Shelter.findByIdAndUpdate(shelterId,{$push: {notes: note[0]._id}})
     await Dogs.findByIdAndUpdate(dogId,{ status: 'Liked' })
+    // else user crea un nuevo mensaje
     res.redirect('/')
     }catch(err){
         next(err)
     }
+})
+
+router.post('/unLikeDog', async (req, res, next) => {
+    // if user tiene al perro en like
+    // else user crea un nuevo mensaje
 })
 
 router.post('/:id', async (req, res, next) => {
@@ -53,7 +37,7 @@ router.post('/:id', async (req, res, next) => {
         const dogId = nota.idDog._id
         const dog = await Dogs.findById(dogId)
         await Notes.findByIdAndUpdate(id,{$push: {message: `${dog.name}: I need more information for adopt you`}})
-        res.redirect('/app/notifications')
+        res.json({message: `${dog.name}: I need more information for adopt you`})
     }catch(err){
         (err)
     }
@@ -65,7 +49,7 @@ router.post('/message/:id', async (req, res, next) => {
         const username = req.session.currentUser.username
         const message = req.body.information
         await Notes.findByIdAndUpdate(id,{$push: {message: `${username}: ${message}`}})
-        res.redirect('/app/notifications')
+        res.json({message: `${username}: ${message}`})
     }catch(err){
         (err)
     }
@@ -80,7 +64,7 @@ router.post('/Dogmessage/:id', async (req, res, next) => {
         const dog = await Dogs.findById(dogId)
         const message = req.body.information
         await Notes.findByIdAndUpdate(id,{$push: {message: `${dog.name}: ${message}`}})
-        res.redirect('/app/notifications')
+        res.json({message: `${dog.name}: ${message}`})
     }catch(err){
         (err)
     }
